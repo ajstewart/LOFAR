a7a9185610d07b7ea2d64c7e3cc03b02154b2f8c
ajstewart <adam.stewart73@gmail.com>
2013-09-30 14:58:21 +0100

    Update gsm_ms.py
    Updated version to 2.1 - you can remove A team sources from a generated model.

--- gsm_ms.py
+++ gsm_ms.py
@@ -2,24 +2,30 @@
 
 import subprocess, optparse, os
 import pyrap.tables as pt
-import math as m
+import numpy as np
 
 usage = "usage: python %prog [options] dataset.MS outfile"
 description="Generate sky model directly from a measurement set."
-vers="1.1"
+vers="2.1"
+
+#Version 2.1 notes
+# Added option to remove Cas and Cyg A.
+
+#Version 2.0 notes
+# Just switched to using numpy.
 
 #Defines the options as present in gsm.py
 parser = optparse.OptionParser(usage=usage, version="%prog v{0}".format(vers), description=description)
 parser.add_option("-r", "--radius", action="store", type="float", dest="radius", default=5., help="Specify cone radius in degrees [default: %default]")
 parser.add_option("-c", "--cutoff", action="store", type="float", dest="cutoff", default=0.1, help="Choose the minimum flux of VLSS sources to use [default: %default]")
 parser.add_option("-t", "--assoctheta", action="store", type="float", dest="assoctheta", default=0.00278, help="Uncertainty in matching in degrees [default: %default (10 arcsec)]")
+parser.add_option("-A", "--removeAteam", action="store_true", dest="removeAteam", default=False, help="Removes Cyg A and Cas A from the model [default: %default]")
 (options, args) = parser.parse_args()
 
 #gsm.py outfile RA DEC radius [vlssFluxCutoff [assocTheta]]
 
-
 def clean(f):
-  input_model=open(f+".temp", 'r')
+	input_model=open(f+".temp", 'r')
 	output_model=open(f, 'w+r')
 	source_names=[]
 	for line in input_model:
@@ -27,6 +33,10 @@ def clean(f):
 		source_name=source[:11]
 		# print source_name
 		if source_name not in source_names:
+			if options.removeAteam:
+				if source_name in Ateam:
+					print "A team source {0} removed".format(source_name)
+					continue
 			source_names.append(source_name)
 			output_model.write(source)
 		else:
@@ -37,6 +47,7 @@ def clean(f):
 	subprocess.call("rm {0}.temp".format(f), shell=True)
 
 
+Ateam=["2323.2+5850", "2323.4+5849", "1959.4+4044"]
 ms=args[0]
 outfile=args[1]
 rad=options.radius
@@ -44,13 +55,11 @@ cut=options.cutoff
 asth=options.assoctheta
 
 print "Obtaining RA and Dec of MS..."
-obs = pt.table(ms + '/FIELD')
-ra = float(obs.col('PHASE_DIR')[0][0][0])
-if ra > 0.:
-	ra*=(180./m.pi)
-else:
-	ra=360.+(ra*(180./m.pi))
-dec = float(obs.col('PHASE_DIR')[0][0][1])*(180./m.pi)
+obs = pt.table(ms + '/FIELD', ack=False)
+ra = np.degrees(float(obs.col('REFERENCE_DIR')[0][0][0]))
+if ra < 0.:
+	ra=360.+(ra)
+dec = np.degrees(float(obs.col('REFERENCE_DIR')[0][0][1]))
 print "RA:{0}\tDec:{1}".format(ra, dec)
 obs.close()
 obs.close()
